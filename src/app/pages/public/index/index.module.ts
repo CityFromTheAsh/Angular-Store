a23f3267f29acfaa1e714c679c06dc13b7f58803
Alexei Matsko <CityFromTheAsh@mail.com>
2019-12-09 01:17:29 +0200

    Add Index Component

--- src/app/pages/public/index/index.module.ts
+++ src/app/pages/public/index/index.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { IndexRoutingModule } from './index-routing.module';
+import { IndexComponent } from './index.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [IndexComponent],
   imports: [
     CommonModule,
     IndexRoutingModule
