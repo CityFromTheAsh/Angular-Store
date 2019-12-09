52d46034762f1f75c7b3cc77263ac014610b7d68
Alexei Matsko <CityFromTheAsh@mail.com>
2019-12-09 13:03:56 +0200

    interrupt

--- src/app/pages/public/index/index.module.ts
+++ src/app/pages/public/index/index.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { IndexRoutingModule } from './index-routing.module';
 import { IndexComponent } from './index.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 @NgModule({
   declarations: [IndexComponent],
   imports: [
     CommonModule,
-    IndexRoutingModule
+    IndexRoutingModule,
+    MatToolbarModule
   ]
 })
 export class IndexModule { }
